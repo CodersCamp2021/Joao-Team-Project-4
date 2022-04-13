0662c1e027bec65e6adfcfe41a6db25599cbaec4
spidero7 <pajak.bartlomiej97@gmail.com>
2022-04-13 21:35:38 +0200

    fix imports

--- backend/routes/adminAccount.ts
+++ backend/routes/adminAccount.ts
@@ -1,6 +1,5 @@
 import * as express from 'express'
 let router = express.Router()
-import { Request, Response } from 'express'
 import User from '../model/User'
 import ROLES_LIST from '../config/roles_list'
 import verifyRoles from '../middleware/verifyRoles'
@@ -8,7 +7,7 @@ import verifyRoles from '../middleware/verifyRoles'
 router.get(
 	'/',
 	verifyRoles(ROLES_LIST.Admin),
-	(req: Request, res: Response) => {
+	(req: express.Request, res: express.Response) => {
 		res.json(req.user)
 		User.findOne({ user: req.user })
 	}
